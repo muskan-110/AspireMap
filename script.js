b4bb63079e2187c3ae764e38c3f4f838a2346390
Jiya022 <jiyac3729@gmail.com>
2024-12-15 13:49:18 +0530

    footer done

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('loginButton').addEventListener('click', function() {
     window.location.href = 'signup';
 });
+
+
+
+    function scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+
